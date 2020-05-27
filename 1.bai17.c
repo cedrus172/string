ea2255c0fa577c6819f75764900c7155932e58c2
cedrus172 <64965926+cedrus172@users.noreply.github.com>
2020-05-27 11:17:55 +0700

    Add files via upload

---
+++ 1.bai17.c
@@ -0,0 +1,22 @@
+﻿#include<stdio.h>
+#include<string.h>
+
+void main()
+{
+	char str1[150];
+	char str2[150];
+	puts("\nNhap string 1 : " );
+	gets(str1);
+	puts("\nNhap string 2 : " );
+	gets(str2);
+
+	if (strstr(str1, str2) != NULL)
+		printf("%s occurs in %s\n", str2, str1);
+	else
+		printf("%s does not occur in %s\n", str2, str1);
+	if (strstr(str2, str1) != NULL)
+		printf("%s occurs in %s\n", str1, str2);
+	else
+		printf("%s does not occur in %s\n", str1, str2);
+
+}
---
+++ 2.bai17.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+void main()
+{
+	int num[5], ctr, sum = 0;
+	int tinhtong(int num_arr[]);
+
+	for (ctr = 0; ctr < 5; ctr++) 
+	{
+		printf("\nNhap so %d: ", ctr + 1);
+
+		scanf("%d", &num[ctr]);
+	}
+	sum = tinhtong(num);
+	printf("\nTrung binh cong cua cac so la is %d", sum);
+
+}
+int tinhtong(int num_arr[])
+{
+	int i, total, trungbinh;
+	for (i = 0, total = 0; i < 5; i++) 
+		total += num_arr[i];
+	trungbinh = total / 5;
+	return trungbinh;
+}
